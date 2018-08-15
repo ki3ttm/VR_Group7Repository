0f24e5abf10dd757a69c7615fd747a1878887b99
Anottidesu <41603090+Anottidesu@users.noreply.github.com>
2018-08-15 14:08:57 +0900

    Add files via upload

---
+++ VR41Group7Project/Assets/Scripts/Ano/HumanMotion/HumanMotion.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanMotion : MonoBehaviour {
+    public enum AnimaList { Wait,Walk,Throw};
+    Animator HumanAnimaData;
+	// Use this for initialization
+	void Start () {
+        HumanAnimaData = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            StartAnimation(AnimaList.Wait);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            StartAnimation(AnimaList.Walk);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            StartAnimation(AnimaList.Throw);
+        }
+    }
+    //各アニメーション再生関数
+    public void StartAnimation(AnimaList AnimaSelect)
+    {
+        switch (AnimaSelect)
+        {
+            case AnimaList.Wait:
+                AllEndAnimation();
+                HumanAnimaData.SetBool("Wait", true);
+                break;
+            case AnimaList.Walk:
+                AllEndAnimation();
+                HumanAnimaData.SetBool("Walk", true);
+                break;
+            case AnimaList.Throw:
+                AllEndAnimation();
+                HumanAnimaData.SetBool("Throw", true);
+                break;
+        }
+    }
+    //各アニメーション停止関数
+    public void EndAnimation(AnimaList AnimaSelect)
+    {
+        switch (AnimaSelect)
+        {
+            case AnimaList.Wait:
+                HumanAnimaData.SetBool("Wait", false);
+                break;
+            case AnimaList.Walk:
+                HumanAnimaData.SetBool("Walk", false);
+                break;
+            case AnimaList.Throw:
+                HumanAnimaData.SetBool("Throw", false);
+                break;
+        }
+    }
+    //全アニメーション停止関数
+    public void AllEndAnimation()
+    {
+        HumanAnimaData.SetBool("Wait", false);
+        HumanAnimaData.SetBool("Walk", false);
+        HumanAnimaData.SetBool("Throw", false);
+    }
+}
---
+++ VR41Group7Project/Assets/Scripts/Ano/Readme.txt
@@ -0,0 +1,6 @@
+HumanMotion.cs
+
+1*HumanのAnimatorと同じinspectorに入れる事
+他のソースにこのHumanに入れたHumanMotion.csを読み込んで使います
+2*他のソースで読み込んだHumanMotionの関数の引数に大体enumで選択して再生させる形になるのでソースの6行目に種類が書いてあるので用途に合わせてお使いください
+3*何か不備等あれば阿野まで
