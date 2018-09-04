38d2991623260bb98fc364488c81652aa5386c7f
liedog <liedogprivate@gmail.com>
2018-09-04 20:03:57 +0900

    シーン遷移の修正とサウンドマネージャーの追加をしました
    サウンドの鳴らし方はスクリプト内でSoundManager.BGMStrat(BGMTitleの列挙子)を書くと鳴ります。
    SEを鳴らすにはSoundManager.SEStrat(SETitleの列挙子 , 鳴らしたい位置(座標)を書くと)なります。
    シーンについてはシーンマネージャーというシーンを突っ込むとタイトルやステージセレクトと切り替えることができます、現在はＡを押すと遷移するようにしています

---
+++ VR41Group7Project/Assets/Scripts/Azuma/Scene/Fade.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour {
+
+	[SerializeField] private float fadeTime = 3.0f;	// フェードする時間
+	float fadeTimeCount = 0.0f;                     // フェードする時間のカウント
+	[SerializeField] private Image imageObj;		// フェードさせるために使う画像
+	// Use this for initialization
+	void Start () {
+		fadeTimeCount = 0.0f;	// 一応初期化
+	}
+
+	/// <summary>
+	/// フェードイン
+	/// </summary>
+	/// <returns></returns>
+	 public IEnumerator FadeIn() {
+		float alpha = 0.0f;
+		for (fadeTimeCount = 0.0f; fadeTimeCount < fadeTime; fadeTimeCount += Time.deltaTime) {
+			alpha = 1.0f - (fadeTimeCount / fadeTime);
+			imageObj.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+			yield return null;
+		}
+		FadeInReset();
+	}
+
+	/// <summary>
+	/// フェードアウト
+	/// </summary>
+	/// <returns></returns>
+	public IEnumerator FadeOut() {
+		float alpha = 0.0f;
+		for (fadeTimeCount = 0.0f; fadeTimeCount < fadeTime; fadeTimeCount += Time.deltaTime) {
+			alpha = fadeTimeCount / fadeTime;
+			imageObj.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+			yield return null;
+		}
+	}
+
+	/// <summary>
+	/// フェードイン時にα値が残ってしまうので0にするための関数
+	/// </summary>
+	private void FadeInReset() {
+		imageObj.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+	}
+}
---
+++ VR41Group7Project/Assets/Scripts/Azuma/Scene/SceneController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	// シーンの名前
+	public enum SceneState {
+		Title,
+		StageSelect,
+		GameMain,
+		Result,
+		MaxScene,
+	}
+
+	string[] sceneName; // シーンの名前を格納
+	bool []sceneFlg;	// シーンを生成中のフラグ
+	bool changeNowFlg;  // チェンジ中のフラグ
+
+	Fade fade;
+
+	private void Awake() {
+		fade = GetComponent<Fade>();
+	
+	}
+
+	// Use this for initialization
+	void Start() {
+		// シーンの名前を初期化
+		sceneName = new string[SceneState.MaxScene.GetHashCode()];
+		sceneName[0] = "Title";
+		sceneName[1] = "StageSelect";
+		sceneName[2] = "GameMain";
+		sceneName[3] = "Result";
+
+		// シーンを生成中のフラグを初期化
+		sceneFlg = new bool[SceneState.MaxScene.GetHashCode()];
+		for (int sequence = 0; sequence < SceneState.MaxScene.GetHashCode(); sequence++) {
+			sceneFlg[sequence] = false;
+		}
+
+		// チェンジ中かのフラグを初期化
+		changeNowFlg = false;
+
+		SceneChange(SceneState.Title);
+
+	}
+
+	/// <summary>
+	/// シーンチェンジをするときに呼び出すもの
+	/// </summary>
+	/// <param name="name"></param>
+	public void SceneChange(SceneState name) {
+		// 2重に入らないようにするための仕掛け
+		if (changeNowFlg) {
+			return;
+		}
+		StartCoroutine(SceneActiveChange(name));
+	}
+
+	/// <summary>
+	/// 遷移開始
+	/// </summary>
+	/// <param name="name"></param>
+	/// <returns></returns>
+	IEnumerator SceneActiveChange(SceneState name) {
+		// 2重に通らないようにフラグを立てておく
+		changeNowFlg = true;
+
+		yield return StartCoroutine(fade.FadeOut());
+
+		// 不要なシーンを削除する
+		for (int sequence = 0; sequence < SceneState.MaxScene.GetHashCode(); sequence++) {
+			if (sceneFlg[sequence] == true) {
+				SceneManager.UnloadSceneAsync(sceneName[sequence]);
+				sceneFlg[sequence] = false;
+			}
+		}
+		// シーンの追加
+		SceneManager.LoadSceneAsync(sceneName[name.GetHashCode()], LoadSceneMode.Additive);
+
+		yield return StartCoroutine(fade.FadeIn());
+
+		sceneFlg[name.GetHashCode()] = true;
+		SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName[name.GetHashCode()]));
+	
+		changeNowFlg = false;
+	}
+
+}
